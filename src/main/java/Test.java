c59aa38c06de19d9e1a82040d483842bc2216b5f
Armin <armin02.kramer@gmail.com>
2017-09-08 14:22:10 +0200

    Added jota lib

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,15 @@
+import jota.IotaAPI;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println(SeedGenerator.generateSeed());
+
+        IotaAPI iotaAPI = new IotaAPI.Builder()
+                .protocol("http")
+                .host("hvo-app.de")
+                .port("14265")
+                .build();
+
+        System.out.println(iotaAPI.getNodeInfo());
     }
 }
