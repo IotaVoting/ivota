45ea658a56bff0a37ea6d00788987a2ddfd84f83
Armin <armin02.kramer@gmail.com>
2017-09-08 15:45:21 +0200

    Test transfer

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,9 @@
 import jota.IotaAPI;
+import jota.dto.response.GetTransactionsToApproveResponse;
+import jota.dto.response.SendTransferResponse;
 import jota.error.*;
 import jota.model.Transfer;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -20,9 +21,15 @@ public class Test {
 
         String testSeed = "OWGZAUFGYFMSGZWEMBHS9JODSDQZYHKTVHZYFTASOSZVWVYJVSQJWMFHTQJIOB9RZ9CRRTL9BBVYSOCCS";
         String testAddress = getAddresses(testSeed, 1).get(0);
-        Transfer transfer = new Transfer(testAddress, 0, "kden", "kdennor");
-        iotaAPI.sendTransfer(testSeed, 2, 15, 15, Collections.singletonList(transfer), null, testAddress);
-        System.out.println(transfer);
+        System.out.println(testAddress);
+        Transfer transfer = new Transfer(testAddress, 0, "KDEN", "IOTALKMESSAGE99999999999999");
+        List<String> preparedTransfers = iotaAPI.prepareTransfers(testSeed, 2, Collections.singletonList(transfer), "", null);
+        System.out.println(preparedTransfers);
+        //GetTransactionsToApproveResponse getTransactionsToApproveResponse = iotaAPI.getTransactionsToApprove(15);
+        //System.out.println(getTransactionsToApproveResponse);
+        SendTransferResponse sendTransferResponse = iotaAPI.sendTransfer(testSeed, 2, 15, 15, Collections.singletonList(transfer), null, testAddress);
+        System.out.println(sendTransferResponse);
+        //iotaAPI.broadcastTransactions(preparedTransfers.get(0));
     }
 
     private static List<String> getAddresses(String seed, int n) throws InvalidAddressException, InvalidSecurityLevelException {
