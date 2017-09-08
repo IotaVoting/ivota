234630264ed08e4b3ea82fc8fc92e09ec34a9f6c
Armin <armin02.kramer@gmail.com>
2017-09-08 14:56:43 +0200

    Test transfer

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,13 +1,15 @@
 import jota.IotaAPI;
-import jota.error.InvalidAddressException;
-import jota.error.InvalidSecurityLevelException;
+import jota.error.*;
+import jota.model.Transfer;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Test {
     static IotaAPI iotaAPI;
 
-    public static void main(String[] args) throws InvalidAddressException, InvalidSecurityLevelException {
+    public static void main(String[] args) throws InvalidAddressException, InvalidSecurityLevelException, InvalidTransferException, InvalidTrytesException, NotEnoughBalanceException {
         System.out.println(SeedGenerator.generateSeed());
 
         iotaAPI = new IotaAPI.Builder()
@@ -16,10 +18,14 @@ public class Test {
                 .port("14265")
                 .build();
 
-        System.out.println(getAdresses(SeedGenerator.generateSeed(), 5));
+        String testSeed = "OWGZAUFGYFMSGZWEMBHS9JODSDQZYHKTVHZYFTASOSZVWVYJVSQJWMFHTQJIOB9RZ9CRRTL9BBVYSOCCS";
+        String testAddress = getAddresses(testSeed, 1).get(0);
+        Transfer transfer = new Transfer(testAddress, 0, "kden", "kdennor");
+        iotaAPI.sendTransfer(testSeed, 2, 15, 15, Collections.singletonList(transfer), null, testAddress);
+        System.out.println(transfer);
     }
 
-    private static List<String> getAdresses(String seed, int n) throws InvalidAddressException, InvalidSecurityLevelException {
+    private static List<String> getAddresses(String seed, int n) throws InvalidAddressException, InvalidSecurityLevelException {
         return iotaAPI.getNewAddress(seed, 2, 0, true, n, false).getAddresses();
     }
 }
